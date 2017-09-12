feadc04d34a90318b8dc965adf0b46324325aed7
guoxiaoyang <guoxiaoyang0925@qq.com>
2017-09-12 23:43:43 +0800

    optimize the response performing

--- src/components/App.js
+++ src/components/App.js
@@ -9,18 +9,9 @@ class Cell extends React.PureComponent {
     this.props.handleClick(this.props.index);
   }
   render() {
-    const dim = this.props.dim;
-    const index = this.props.index;
-    const height = this.props.height;
-    const x = Number.parseInt(index/height, 10);
-    const y = index - x * height;
-    const cellStyle = {
-      fill: this.props.cellState === 1 ? '#4D78CC' : '#FFF',
-      strokeWidth: '1px',
-      stroke: '#000'
-    };
+    const className = this.props.cellState === 1 ? 'cell live' : 'cell';
     return (
-      <rect width={dim} height={dim} x={x * dim} y={y * dim} onClick={() => this.handleClick()} style={cellStyle}/>
+      <div className={className} onClick={() => this.handleClick()}/>
     );
   }
 }
@@ -35,19 +26,19 @@ Cell.propTypes = {
 class GameBoard extends React.PureComponent {
   constructor() {
     super();
-    this.dim = 10;
+    // this.dim = 10;
   }
 
   render() {
-    const dim = this.dim;
+    // const dim = this.dim;
     const height = this.props.boardHeight;
-    const pixelWidth = this.props.boardWidth * dim;
-    const pixelHeight = this.props.boardHeight * dim;
+    // const pixelWidth = this.props.boardWidth * dim;
+    // const pixelHeight = this.props.boardHeight * dim;
     const cellStates = this.props.cellStates;
     return (
-      <svg width={pixelWidth.toString()} height={pixelHeight.toString()}>
-        {cellStates.map((cellState, index) => <Cell dim={dim} height={height} cellState={cellState} index={index} key={index} handleClick={this.props.handleClick}/>)}
-      </svg>
+      <div className = "game-board">
+        {cellStates.map((cellState, index) => <Cell cellState={cellState} index={index} key={index} handleClick={this.props.handleClick}/>)}
+      </div>
     );
   }
 }
